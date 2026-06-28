4ef60590c523febc020c6263bbd34e259b012d3e
Yoga pratama <y4.yogapratama123@gmail.com>
2026-06-29 03:30:48 +0700

    Resize icons to exact squares to fix Android Chrome strict sizing

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 {
-	"version": 1782400026,
+	"version": 1782400027,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
---
+++ resize.ps1
@@ -0,0 +1,32 @@
+Add-Type -AssemblyName System.Drawing
+$sourceFile = "d:\Users\Downloads\Last Unit\logo.png"
+$sourceImg = [System.Drawing.Image]::FromFile($sourceFile)
+
+$sizes = @(16, 32, 114, 128, 256, 512)
+
+foreach ($size in $sizes) {
+    $bmp = New-Object System.Drawing.Bitmap($size, $size)
+    $g = [System.Drawing.Graphics]::FromImage($bmp)
+    
+    # Draw a black background since the logo is likely designed for dark themes
+    $g.Clear([System.Drawing.Color]::Black)
+    
+    # Maintain aspect ratio and center
+    $ratio = [math]::Min($size / $sourceImg.Width, $size / $sourceImg.Height)
+    $newWidth = [int]($sourceImg.Width * $ratio)
+    $newHeight = [int]($sourceImg.Height * $ratio)
+    
+    $x = [int](($size - $newWidth) / 2)
+    $y = [int](($size - $newHeight) / 2)
+    
+    $g.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+    $g.DrawImage($sourceImg, $x, $y, $newWidth, $newHeight)
+    
+    $targetFile = "d:\Users\Downloads\Last Unit\icon-$size.png"
+    $bmp.Save($targetFile, [System.Drawing.Imaging.ImageFormat]::Png)
+    
+    $g.Dispose()
+    $bmp.Dispose()
+}
+
+$sourceImg.Dispose()
